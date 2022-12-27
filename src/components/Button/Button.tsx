44e84d0829a634d1efea0cc64c1e24491efd8749
StombieIT <stombie@yandex.ru>
2022-12-27 14:10:10 +0300

    create button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames"
+import { FC, ButtonHTMLAttributes } from "react"
+
+import classes from "./Button.module.css"
+
+export enum ButtonSubType {
+    PRIMARY = "primary",
+    SECONDARY = "secondary",
+    SUCCESS = "success",
+    DANGER = "danger",
+    WARNING = "warning",
+    LIGHT = "light",
+    DARK = "dark"
+}
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    subType: ButtonSubType
+}
+
+export const Button: FC<IButtonProps> = ({subType, className, children, ...props}) => {
+    const buttonClass = classNames(className, {
+        [classes.common]: classes.common,
+    })
+
+    return <button {...props} className={ buttonClass }>
+        { children }
+    </button>
+}
