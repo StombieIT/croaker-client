ba05bb9474a745c530fc7d6643eb9c3873eeefc1
StombieIT <stombie@yandex.ru>
2022-12-27 13:45:37 +0300

    add sidebar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,10 @@
-import { FC, useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
-import { NotificationType } from "../../models/NotificationType"
-import { createAddNotificationAction } from "../../reducers/notificationsReducer/notificationsReducer"
+import { FC } from "react"
 import { Modal } from "../Modal/Modal"
+import { SideBar } from "../SideBar/SideBar"
 
 export const App: FC = () => {
-    const dispatch = useDispatch()
-    const [text, setText] = useState<string>("")
-    const [type, setType] = useState<NotificationType>(NotificationType.INFO)
-
-    const add = () => dispatch(createAddNotificationAction({text, type}))
-
     return <div>
-        <input value={ text } onChange={ evt => setText(evt.target.value) } />
-        <input name="ficko" type="radio" value={NotificationType.INFO} onChange={ evt => setType(evt.target.value as NotificationType) } />
-        <input name="ficko" type="radio" value={NotificationType.SUCCESS} onChange={ evt => setType(evt.target.value as NotificationType) } />
-        <input name="ficko" type="radio" value={NotificationType.ERROR} onChange={ evt => setType(evt.target.value as NotificationType) } />
-        <button onClick={add}>Click</button>
+        <SideBar />
         <Modal />
     </div>
 }
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react"
+
+export const SideBar: FC = () => {
+    return <aside>
+    </aside>
+}
