f02ac2fb6463e8d9b717efd481e86af413aa711a
StombieIT <stombie@yandex.ru>
2022-12-30 18:08:13 +0300

    add some tests + implement some tests

--- src/components/PathTriggerLink/PathTriggerLink.test.tsx
+++ src/components/PathTriggerLink/PathTriggerLink.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react"
-import { describe, test } from "node:test"
+import { BrowserRouter } from "react-router-dom"
 import { PathTriggerLink } from "./PathTriggerLink"
 
 describe("PathTriggerLink component tests", () => {
@@ -7,17 +7,39 @@ describe("PathTriggerLink component tests", () => {
     const linkClassName: string = "some-class"
     const path: string = "/some-path"
 
+    test("component uses children")
+    
     test("class attribute does not exist when component is not triggered", () => {
         render(
-            <PathTriggerLink
-                to={ path }
-                when={ path => false }
-                className={ isTriggered => isTriggered ? linkClassName : undefined }
-            >
-                { linkText }
-            </PathTriggerLink>
+            <BrowserRouter>
+                <PathTriggerLink
+                    to={ path }
+                    when={ path => false }
+                    className={ isTriggered => isTriggered ? linkClassName : undefined }
+                >
+                    { linkText }
+                </PathTriggerLink>
+            </BrowserRouter>
         )
         
-        expect(screen.getByText(linkText).className).toBeUndefined()
+        expect(screen.getByText(linkText).className).toBeFalsy()
     })
+
+    test("class attribute exists when component is triggered", () => {
+        render(
+            <BrowserRouter>
+                <PathTriggerLink
+                    to={ path }
+                    when={ path => true }
+                    className={ isTriggered => isTriggered ? linkClassName : undefined }
+                >
+                    { linkText }
+                </PathTriggerLink>
+            </BrowserRouter>
+        )
+
+        expect(screen.getByText(linkText).className).toEqual(linkClassName)
+    })
+
+    test("style attribute overrides component's styles")
 })
