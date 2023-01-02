8d6a18431550fa63dab7539dab57f3c927071f4c
StombieIT <stombie@yandex.ru>
2023-01-03 00:33:05 +0300

    implement main tests for path trigger link component

--- src/components/PathTriggerLink/PathTriggerLink.test.tsx
+++ src/components/PathTriggerLink/PathTriggerLink.test.tsx
@@ -2,12 +2,27 @@ import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import { PathTriggerLink } from "./PathTriggerLink"
 
+import classes from "Test.module.css"
+import { CSSProperties } from "react"
+
 describe("PathTriggerLink component tests", () => {
     const linkText: string = "some-link-text"
     const linkClassName: string = "some-class"
     const path: string = "/some-path"
 
-    test("component uses children")
+    test("component uses children", () => {
+        render(
+            <BrowserRouter>
+                <PathTriggerLink
+                    to={ path }
+                >
+                    { linkText }
+                </PathTriggerLink>
+            </BrowserRouter>
+        )
+
+        expect(screen.getByText(linkText)).toBeDefined()
+    })
     
     test("class attribute does not exist when component is not triggered", () => {
         render(
@@ -41,5 +56,25 @@ describe("PathTriggerLink component tests", () => {
         expect(screen.getByText(linkText).className).toEqual(linkClassName)
     })
 
-    test("style attribute overrides component's styles")
+    test("style attribute overrides component's styles", () => {
+        const linkStyle: CSSProperties = {
+            backgroundColor: "green"
+        }
+        
+        render(
+            <BrowserRouter>
+                    <PathTriggerLink
+                        to={ path }
+                        className={ isTriggered => classes.common }
+                        style={ isTriggered => linkStyle }
+                    >
+                        { linkText }
+                    </PathTriggerLink>
+            </BrowserRouter>
+        )
+        
+        const link: HTMLElement = screen.getByText(linkText)
+        
+        expect(link.style.backgroundColor).toBe(linkStyle.backgroundColor)
+    }) 
 })
