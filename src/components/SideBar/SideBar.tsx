fafe7ccbd7a10d955e27be3af0fb2e2391aa77d8
StombieIT <stombie@yandex.ru>
2023-01-03 19:59:24 +0300

    remove path trigger link component from project

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,13 +1,6 @@
 import { FC } from "react"
-import { PathTriggerLink } from "../PathTriggerLink/PathTriggerLink"
 
 export const SideBar: FC = () => {
     return <aside>
-        <PathTriggerLink
-            style={ isTriggered => isTriggered ? {backgroundColor: "red"} : undefined }
-            to="/ficko"
-        >
-            Some link
-        </PathTriggerLink>
     </aside>
 }
