9e9b5c00fc5a08bc3138d46a3021eba93061a279
StombieIT <stombie@yandex.ru>
2023-01-09 01:02:37 +0300

    add media grid component

---
+++ src/components/MediaGrid/MediaGrid.tsx
@@ -0,0 +1,20 @@
+import classNames from "classnames"
+import { FC, Children, ReactNode } from "react"
+import { Media } from "../Media/Media"
+
+import classes from "./MediaGrid.module.scss"
+
+interface IMediaGridProps {
+    children: Array<ReactNode>,
+    className?: string
+}
+
+export const MediaGrid: FC<IMediaGridProps> = ({children, className}) => {
+    const gridClass: string = classNames(className, classes.grid)
+    
+    return <div className={ gridClass }>
+        {
+            Children.map(children, (child, idx) => <Media className={ classes.wrapper }>{ child }</Media>)
+        }
+    </div>
+}
