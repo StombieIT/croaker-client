04bc5235a320a704d6a92329a6a160043a292aa5
StombieIT <stombie@yandex.ru>
2023-01-09 18:40:53 +0300

    add croaks reducer to application

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
+import { croaksReducer } from "./reducers/croaks/croaksReducer"
 import { notificationsReducer } from "./reducers/notifications/notificationsReducer"
 
 export const store = configureStore({
     reducer: {
-        notifications: notificationsReducer
+        notifications: notificationsReducer,
+        croaks: croaksReducer
     }
 })
 
