e4504c3dba146f496d51154f2a18a8626d905bd9
StombieIT <stombie@yandex.ru>
2023-01-09 21:34:10 +0300

    refactor notifications container with new slice-action-creators

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -2,7 +2,7 @@ import { Dispatch } from "@reduxjs/toolkit"
 import { FC } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { INotification } from "../../models/INotification"
-import { createRemoveNotificationAction } from "../../reducers/notifications/notificationsReducer"
+import { removeNotification } from "../../reducers/notifications/notificationsSlice"
 import { selectNotificationsList } from "../../reducers/notifications/notificationsSelectors"
 import { Notification } from "../Notification/Notification"
 
@@ -35,11 +35,9 @@ const NotificationsContainer: FC<INotificationContainerProps> = ({}) => {
     const notificationsList: Array<INotification> = useSelector(selectNotificationsList)
     const dispatch: Dispatch = useDispatch()
 
-    const removeNotification = (id: string) => dispatch(createRemoveNotificationAction(id))
-
     return <Notifications
         list={ notificationsList }
-        removeNotification={ removeNotification }
+        removeNotification={ (id: string) => dispatch(removeNotification(id)) }
     />
 }
 
