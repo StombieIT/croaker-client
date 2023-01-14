41806ef12d0e080a3674e4afcd3571569b2f75bd
StombieIT <stombie@yandex.ru>
2023-01-14 15:06:43 +0300

    add memoization for formatted date

--- src/components/PassedTime/PassedTime.tsx
+++ src/components/PassedTime/PassedTime.tsx
@@ -1,13 +1,15 @@
-import { FC, HTMLAttributes } from "react"
+import { FC, HTMLAttributes, useMemo } from "react"
 import { DateJson } from "../../models/DateJson"
-import { formatDate } from "./utils"
+import { formatDate } from "../../utils/formatDate"
 
 interface IPassedTimeProps extends HTMLAttributes<HTMLTimeElement> {
     children: DateJson | number
 }
 
 export const PassedTime: FC<IPassedTimeProps> = ({children, ...props}) => {
+    const formattedDate = useMemo<string>(() => formatDate(new Date(children)), [])
+
     return <time {...props} >
-        { formatDate(new Date(children)) }
+        { formattedDate }
     </time>
 }
