dbde317d819ff2daf024a391a7f689b7591f1ee7
StombieIT <stombie@yandex.ru>
2023-01-14 18:02:07 +0300

    add redux-saga middleware to store

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit"
+import createSagaMiddleware from "redux-saga"
 import { croaksReducer } from "./business-logic/croaks/croaksSlice"
 import { notificationsReducer } from "./business-logic/notifications/notificationsSlice"
+import { rootSaga } from "./business-logic/rootSaga"
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
     reducer: {
         notifications: notificationsReducer,
         croaks: croaksReducer
-    }
+    },
+    middleware: getDefaultMiddleware => [
+        ...getDefaultMiddleware({thunk: false}),
+        sagaMiddleware
+    ]
 })
+
+sagaMiddleware.run(rootSaga)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
