ed482411d0493fa8c8978d24070523280c7aa9f1
StombieIT <stombie@yandex.ru>
2023-01-15 19:08:15 +0300

    add profile meta component

---
+++ src/components/ProfileMeta/ProfileMeta.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react"
+
+import { DateJson } from "../../models/DateJson"
+
+import classes from "./ProfileMeta.module.scss"
+import locationIcon from "./locationIcon.svg"
+import calendarIcon from "./calendarIcon.svg"
+import { formatDateToMonthAndYear } from "../../utils/formatDateToMonthAndYear"
+
+interface IProfileMetaProps {
+    userId: number,
+    city: string,
+    country: string,
+    registrationDate: DateJson
+}
+
+export const ProfileMeta: FC<IProfileMetaProps> = ({
+    userId,
+    city,
+    country,
+    registrationDate
+}) => {
+    return <div className={ classes.container }>
+        <div className={ classes.element }>
+            User id: <b className={ classes.bold }>{ userId }</b>
+        </div>
+        <div className={ classes.withIcon }>
+            <img src={ locationIcon } alt="Location icon" />
+            <div>
+                <b className={ classes.bold }>{ country }</b>, { city }
+            </div>
+        </div>
+        <div className={ classes.withIcon }>
+            <img src={ calendarIcon } alt="Calendar icon" />
+            <div>
+                Member scince <b className={ classes.bold }>{ formatDateToMonthAndYear(new Date(registrationDate)) }</b>
+            </div>
+        </div>
+    </div>
+}
