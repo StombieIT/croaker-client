3d2955e8070d298bdbaf60e95fc40e998f8d0c7c
StombieIT <stombie@yandex.ru>
2023-01-17 23:03:33 +0300

    remove business-logic from component, add routes

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,16 @@
 import { FC, useEffect } from "react"
+import { Routes, Route } from "react-router-dom"
 import { NavLink } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { selectProfileState } from "../../business-logic/profile/profileSelectors"
-import { IProfileState, setProfile } from "../../business-logic/profile/profileSlice"
+import { fetchProfile, IProfileState, setProfile } from "../../business-logic/profile/profileSlice"
 import { AppDispatch } from "../../store"
 import { ProfileHeader } from "../ProfileHeader/ProfileHeader"
-import { IUser } from "../../models/IUser"
-import { IProfile } from "../../models/IProfile"
-import { DateJson } from "../../models/DateJson"
 import { ImageWrapper, ImageWrapperType } from "../ImageWrapper/ImageWrapper"
 import { ProfileInteractionBar } from "../ProfileInteractionBar/ProfileInteractionBar"
 import { ProfileUserInfo } from "../ProfileInfo/ProfileInfo"
 import { NavBar } from "../NavBar/NavBar"
+import Croaks from "../Croaks/Croaks"
 
 import classes from "./Profile.module.scss"
 
@@ -60,6 +59,9 @@ export const Profile: FC<IProfileProps> = ({state}) => {
                 Likes
             </NavLink>
         </NavBar>
+        <Routes>
+            <Route path="/croaks" element={ <Croaks /> } />
+        </Routes>
     </main>
 }
 
@@ -67,27 +69,8 @@ const ProfileContainer: FC<IProfileContainerProps> = ({}) => {
     const state: IProfileState = useSelector(selectProfileState)
     const dispatch: AppDispatch = useDispatch()
 
-    const dio: IUser = {
-        id: 1,
-        username: "dio_brando",
-        name: "Dio Brando",
-        avatarLink: "https://i.ytimg.com/vi/CGoPIsbnGFY/maxresdefault.jpg",
-        registrationDate: new Date().toJSON() as DateJson
-    }
-
-    const dioProfile: IProfile = {
-        user: dio,
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione nisi beatae architecto fuga nobis ipsam voluptatem recusandae non quibusdam alias doloribus esse incidunt cumque dolor hic corrupti aliquid nulla excepturi ab veritatis velit quas, cum facilis? Suscipit, totam debitis? Mollitia, deserunt? Repudiandae molestias quasi iusto amet, totam asperiores ut facilis?",
-        city: "Kair",
-        country: "Egypt",
-        croaksCount: 392,
-        followersCount: 1337,
-        followingCount: 1,
-        backgroundImageLink: "https://animetree.files.wordpress.com/2015/06/jjba-stardust-crusaders-sunset-over-cairo.jpg"
-    }
-
     useEffect(() => {
-        dispatch(setProfile(dioProfile))
+        dispatch(fetchProfile(1))
     }, [])
 
     return <Profile state={ state } />
