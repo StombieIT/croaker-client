6334e5ce6170dc7073d2741b578574fce608c628
StombieIT <stombie@yandex.ru>
2023-01-17 23:03:40 +0300

    add routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react"
+import { Routes, Route } from "react-router-dom"
 import { SideBar } from "../SideBar/SideBar"
-import { Profile } from "../Profile/Profile"
 import { NarrowColumn } from "../NarrowColumn/NarrowColumn"
+import Profile from "../Profile/Profile"
 
 import classes from "./App.module.css"
 
 export const App: FC = () => {
     return <div className={ classes.common }>
         <SideBar />
-        <Profile />
+        <Routes>
+            <Route path="/profile/*" element={ <Profile /> } />
+        </Routes>
         <NarrowColumn />
     </div>
 }
