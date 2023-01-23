90cd6a51f01ccc7141874a13e9a23566e57e9be6
StombieIT <stombie@yandex.ru>
2023-01-23 17:44:40 +0300

    add preloader component

---
+++ src/components/PreLoader/PreLoader.tsx
@@ -0,0 +1,20 @@
+import classNames from "classnames"
+import { FC, HTMLAttributes } from "react"
+
+import classes from "./PreLoader.module.css"
+import preloaderIcon from "./preloaderIcon.png"
+
+interface IPreLoaderProps extends HTMLAttributes<HTMLImageElement> {
+    className?: string
+}
+
+export const PreLoader: FC<IPreLoaderProps> = ({className, ...props}) => {
+    const preloaderClass: string = classNames(classes.icon, className)
+    
+    return <img
+        {...props}
+        className={ preloaderClass }
+        src={ preloaderIcon }
+        alt="PreLoader"
+    />
+}
