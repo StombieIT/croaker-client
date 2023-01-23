9afbd130fb5315caf708401654e0762569fd8cbd
StombieIT <stombie@yandex.ru>
2023-01-23 13:46:04 +0300

    move index.css and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
 import { App } from "./components/App/App"
 import { NotificationsPopup } from "./components/NotificationsPopup/NotificationsPopup"
 import { store } from "./store"
-import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
+import "./index.css"
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
