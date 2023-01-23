3ce84abff00c49b09912b9ffbf016b16b670c90d
StombieIT <stombie@yandex.ru>
2023-01-23 19:34:16 +0300

    add croaks paginator selector

--- src/business-logic/croaks/croaksSelectors.ts
+++ src/business-logic/croaks/croaksSelectors.ts
@@ -1,4 +1,8 @@
+import { ICroak } from "../../models/ICroak"
+import { IPaginator } from "../../models/IPaginator"
 import { RootState } from "../../store"
 import { ICroaksState } from "./croaksSlice"
 
 export const selectCroaksState = (state: RootState): ICroaksState => state.croaks
+
+export const selectCroaksPaginator = (state: RootState): IPaginator<ICroak> | undefined => state.croaks.paginator
