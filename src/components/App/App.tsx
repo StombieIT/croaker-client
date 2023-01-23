31451a5b8ec67b93d8635ccde95aa1a62f730614
StombieIT <stombie@yandex.ru>
2023-01-23 18:49:54 +0300

    fix app routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { SideBar } from "../SideBar/SideBar"
 import { NarrowColumn } from "../NarrowColumn/NarrowColumn"
 import Profile from "../Profile/Profile"
+import ErrorBanner from "../ErrorBanner/ErrorBanner"
 
 import classes from "./App.module.css"
 
@@ -10,7 +11,9 @@ export const App: FC = () => {
     return <div className={ classes.common }>
         <SideBar />
         <Routes>
-            <Route path="/profile/*" element={ <Profile /> } />
+            <Route path="/profile/:id" element={ <Profile /> } />
+            <Route path="/error/:heading" element={ <ErrorBanner /> } />
+            <Route path="/*" element={ <Navigate to="/error/404" /> } />
         </Routes>
         <NarrowColumn />
     </div>
