f0e673934b421c3b72b2bba05777138cc74795f2
StombieIT <stombie@yandex.ru>
2023-01-23 19:35:14 +0300

    fix profile saga

--- src/business-logic/profile/profileSaga.ts
+++ src/business-logic/profile/profileSaga.ts
@@ -1,15 +1,22 @@
-import { takeEvery, call, put, StrictEffect } from "@redux-saga/core/effects"
+import { takeEvery, call, all, put, StrictEffect } from "@redux-saga/core/effects"
 import { PayloadAction } from "@reduxjs/toolkit"
-import { getProfileByUserId } from "../../api/profilesApi"
+import { getProfileById } from "../../api/profilesApi"
 import { IProfile } from "../../models/IProfile"
-import { fetchProfile, setProfile } from "./profileSlice"
+import { NotificationType } from "../../models/NotificationType"
+import { appendNotificationContainer } from "../notifications/notificationsSlice"
+import { fetchProfileById, setIsLoading, setProfile } from "./profileSlice"
 
 export function *profileWatcher(): Generator<StrictEffect> {
-    yield takeEvery(fetchProfile.type, profileWorker)
+    yield takeEvery(fetchProfileById.type, profileWorker)
 }
 
-export function *profileWorker(action: PayloadAction<number>): Generator<StrictEffect, void, IProfile> {
-    const profile: IProfile = yield call(getProfileByUserId, action.payload)
-
-    yield put(setProfile(profile))
+export function *profileWorker(action: PayloadAction<number>) {
+    yield put(setIsLoading(true))
+    try {
+        const profile: IProfile = yield call(getProfileById, action.payload)
+        yield put(setProfile(profile))
+        yield put(setIsLoading(false))
+    } catch (err) {
+        yield put(appendNotificationContainer({type: NotificationType.ERROR, text: "Unable to load profile"}))
+    }
 }
