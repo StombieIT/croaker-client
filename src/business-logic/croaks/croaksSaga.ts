49bcd7ce7ea61a7225f3a519ccb21c2b493a9a41
StombieIT <stombie@yandex.ru>
2023-01-28 19:36:17 +0300

    fix existing worker and add fetching original croak method

--- src/business-logic/croaks/croaksSaga.ts
+++ src/business-logic/croaks/croaksSaga.ts
@@ -1,26 +1,40 @@
-import { select, call, put, takeEvery } from "@redux-saga/core/effects"
+import { select, call, put, takeLatest, takeLeading } from "@redux-saga/core/effects"
 import { PayloadAction } from "@reduxjs/toolkit"
-import { getCroaksByUserId } from "../../api/croaksApi"
-import { ICroak } from "../../models/ICroak"
 import { IPaginator } from "../../models/IPaginator"
+import { fromDto, ICroak } from "../../models/ICroak"
+import { ICroakDto } from "../../models/ICroakDto"
 import { NotificationType } from "../../models/NotificationType"
 import { appendNotificationContainer } from "../notifications/notificationsSlice"
 import { selectCroaksPaginator } from "./croaksSelectors"
-import { fetchNextCroaksByUserId, mergePaginator, setIsLoading } from "./croaksSlice"
+import {
+    fetchNextCroaksByUserId, fetchOriginalCroakById,
+    mergePaginator, setIsLoading, garbleOriginalCroak
+} from "./croaksSlice"
+import * as croaksApi from "../../api/croaksApi"
 
 export function *croaksWatcher() {
-    yield takeEvery(fetchNextCroaksByUserId.type, croaksWorker)
+    yield takeLatest(fetchNextCroaksByUserId.type, fetchNextCroaksByUserIdWorker)
+    yield takeLeading(fetchOriginalCroakById.type, fetchOriginalCroakByIdWorker)
 }
 
-export function *croaksWorker(action: PayloadAction<number>) {
+export function *fetchNextCroaksByUserIdWorker(action: PayloadAction<number>) {
     yield put(setIsLoading(true))
     try {
         const croaksPaginator: IPaginator<ICroak> | undefined = yield select(selectCroaksPaginator)
         const nextPage: number = croaksPaginator ? croaksPaginator.page + 1 : 1
-        const nextCroaksPaginator: IPaginator<ICroak> = yield call(getCroaksByUserId, action.payload, nextPage)
-        yield put(mergePaginator(nextCroaksPaginator))
+        const nextCroaksPaginator: IPaginator<ICroakDto> = yield call(croaksApi.getCroaksByUserId, action.payload, nextPage)
+        yield put(mergePaginator({...nextCroaksPaginator, items: nextCroaksPaginator.items.map(fromDto)}))
         yield put(setIsLoading(false))
     } catch (err) {
         yield put(appendNotificationContainer({type: NotificationType.ERROR, text: "Unable to load croaks ;("}))
+    }
+}
+
+export function *fetchOriginalCroakByIdWorker(action: PayloadAction<number>) {
+    try {
+        const croak: ICroakDto = yield call(croaksApi.getCroakById, action.payload)
+        yield put(garbleOriginalCroak(fromDto(croak)))
+    } catch (err) {
+        yield put(appendNotificationContainer({type: NotificationType.ERROR, text: "Unable to load original croak ;("}))    
     }
 }
