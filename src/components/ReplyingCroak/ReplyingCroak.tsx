77efab8315d08a5518168cc2f23c7c2d9a463780
StombieIT <stombie@yandex.ru>
2023-01-28 19:40:10 +0300

    add replying croak model

---
+++ src/components/ReplyingCroak/ReplyingCroak.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import { Croak, ICroakProps } from "../Croak/Croak"
+import ReplyingFor from "../ReplyingFor/ReplyingFor"
+
+import classes from "./ReplyingCroak.module.scss"
+
+export const ReplyingCroak: FC<ICroakProps> = (props) => {
+    return <div className={ classes.container }>
+        <ReplyingFor croakId={ props.croak.id } />
+        <Croak {...props} />
+    </div>
+}
