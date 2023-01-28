72a7bae0c74ada89183491939ced3bdbd2e93623
StombieIT <stombie@yandex.ru>
2023-01-28 19:36:39 +0300

    add selectors for original croak

--- src/business-logic/croaks/croaksSelectors.ts
+++ src/business-logic/croaks/croaksSelectors.ts
@@ -1,4 +1,6 @@
+import { IActivable } from "../../models/IActivable"
 import { ICroak } from "../../models/ICroak"
+import { IIdentifiable } from "../../models/IIdentifiable"
 import { IPaginator } from "../../models/IPaginator"
 import { RootState } from "../../store"
 import { ICroaksState } from "./croaksSlice"
@@ -6,3 +8,18 @@ import { ICroaksState } from "./croaksSlice"
 export const selectCroaksState = (state: RootState): ICroaksState => state.croaks
 
 export const selectCroaksPaginator = (state: RootState): IPaginator<ICroak> | undefined => state.croaks.paginator
+
+export const selectIsFilledOriginalCroakById = (id: number) => (state: RootState): boolean =>
+    state.croaks
+        .paginator
+        ?.items
+        .filter(croak => croak.originalCroak?.id === id)
+        .every(croak => !!croak.originalCroak?.text)
+        ?? false
+
+export const selectOriginalCroakOfCroakById = (id: number) => (state: RootState): Partial<ICroak> & IActivable & IIdentifiable | undefined =>
+    state.croaks
+        .paginator
+        ?.items
+        .find(croak => croak.id === id)
+        ?.originalCroak
