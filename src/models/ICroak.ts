9000ce44626024ae078d0d58d5bad550396add17
StombieIT <stombie@yandex.ru>
2023-01-28 19:34:19 +0300

    fix croak model and add type checking method

--- src/models/ICroak.ts
+++ src/models/ICroak.ts
@@ -1,8 +1,33 @@
+import { IActivable } from "./IActivable"
 import { ICroakBase } from "./ICroakBase"
+import { ICroakDto } from "./ICroakDto"
+import { IIdentifiable } from "./IIdentifiable"
 import { IReaction } from "./IReaction"
 
 export interface ICroak extends ICroakBase {
     likes: IReaction,
     comments: IReaction,
-    replies: IReaction
+    replies: IReaction,
+    originalCroak?: Partial<ICroak> & IActivable & IIdentifiable
+}
+
+export const isICroak = (croak: Partial<ICroak>): croak is ICroak =>
+    croak.id !== undefined &&
+    croak.text !== undefined &&
+    croak.creationDate !== undefined &&
+    croak.author !== undefined &&
+    croak.imagesLinks !== undefined &&
+    croak.likes !== undefined &&
+    croak.comments !== undefined &&
+    croak.replies !== undefined
+
+export const fromDto = (croak: ICroakDto): ICroak => {
+    const {originalCroakId, ...croakCutted} = croak
+    return {
+        ...croakCutted,
+        likes: {...croak.likes, isLoading: false},
+        comments: {...croak.comments, isLoading: false},
+        replies: {...croak.replies, isLoading: false},
+        originalCroak: croak.originalCroakId ? {isActive: false, id: croak.originalCroakId} : undefined
+    }
 }
