3f5f9457208a3ab988b701d13b829b59a96f7d79
StombieIT <stombie@yandex.ru>
2023-01-28 19:33:26 +0300

    add new methods in croaks api

--- src/api/croaksApi.ts
+++ src/api/croaksApi.ts
@@ -1,7 +1,15 @@
-import { ICroak } from "../models/ICroak"
+import { ICroakDto } from "../models/ICroakDto"
 import { IPaginator } from "../models/IPaginator"
 import { api } from "./api"
 
-export const getCroaksByUserId = (userId: number, page: number = 1, limit: number = 10) =>
-    api.get<IPaginator<ICroak>>(`/croaks/by-user-id/${userId}?page=${page}&limit=${limit}`)
-    .then(response => response.data)
+export const getCroaksByUserId = (userId: number, page: number = 1, limit: number = 10): Promise<IPaginator<ICroakDto>> =>
+    api.get<IPaginator<ICroakDto>>(`/croaks/by-user-id/${userId}?page=${page}&limit=${limit}`)
+        .then(response => response.data)
+
+export const getRepliesByUserId = (userId: number, page: number = 1, limit: number = 10): Promise<IPaginator<ICroakDto>> =>
+    api.get<IPaginator<ICroakDto>>(`/croaks/replies/by-user-id/${userId}?page=${page}&limit=${limit}`)
+        .then(response => response.data)
+
+export const getCroakById = (id: number): Promise<ICroakDto> =>
+    api.get<ICroakDto>(`/croaks/${id}`)
+        .then(response => response.data)
