7a80aabde83eb45d709c410d8b1d353eda540904
StombieIT <stombie@yandex.ru>
2023-01-30 01:25:23 +0300

    add some tests for login component

---
+++ src/components/Login/Login.test.tsx
@@ -0,0 +1,88 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import { Login } from "./Login"
+import { ILoginFormState } from "../../business-logic/loginForm/loginFormSlice"
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    Link: () => null
+}))
+
+describe("Login component tests", () => {
+    const mockedUsernameChange = jest.fn()
+    const mockedPasswordChange = jest.fn()
+    let form: ILoginFormState
+
+    beforeEach(() => {
+        form = {
+            isValid: false,
+            username: {
+                value: "",
+                errors: []
+            },
+            password: {
+                value: "",
+                errors: []
+            }
+        }
+
+    })
+
+    test("fields changing", () => {
+        render(
+            <Login
+                form={ form }
+                onUsernameChange={ mockedUsernameChange }
+                onPasswordChange={ mockedPasswordChange }
+            />
+        )
+
+        const username = screen.getByPlaceholderText(/username/i)
+        const typedUsername: string = "ficko"
+        fireEvent.change(username, {
+            target: {value: typedUsername}
+        })
+        expect(mockedUsernameChange).toBeCalledWith(typedUsername)
+
+        const password = screen.getByPlaceholderText(/password/i)
+        const typedPassword: string = "123456"
+        fireEvent.change(password, {
+            target: {value: typedPassword}
+        })
+        expect(mockedPasswordChange).toBeCalledWith(typedPassword)
+    })
+
+    test("submitting valid form", () => {
+        form.isValid = true
+        const mockedFormSubmit = jest.fn()
+
+        render(
+            <Login
+                form={ form }
+                onUsernameChange={ mockedUsernameChange }
+                onPasswordChange={ mockedPasswordChange }
+                onFormSubmit={ mockedFormSubmit }
+            />
+        )
+
+        fireEvent.click(screen.getByRole("button"))
+
+        expect(mockedFormSubmit).toBeCalled()
+    })
+
+    test("submitting invalid form", () => {
+        const mockedFormSubmit = jest.fn()
+        
+        render(
+            <Login
+                form={ form }
+                onUsernameChange={ mockedUsernameChange }
+                onPasswordChange={ mockedPasswordChange }
+                onFormSubmit={ mockedFormSubmit }
+            />
+        )
+
+        fireEvent.click(screen.getByRole("button"))
+
+        expect(mockedFormSubmit).not.toBeCalled()
+    })
+})
