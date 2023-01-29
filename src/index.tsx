01dbabe8e7661ddff91c40dedf53916a44f60469
StombieIT <stombie@yandex.ru>
2023-01-29 17:52:30 +0300

    activate skeleton library opportunities

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
+import { SkeletonTheme } from "react-loading-skeleton"
 import App from "./components/App/App"
 import { NotificationsPopup } from "./components/NotificationsPopup/NotificationsPopup"
 import { store } from "./store"
+
 import "./index.css"
+import "react-loading-skeleton/dist/skeleton.css"
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -13,8 +16,10 @@ const root = ReactDOM.createRoot(
 root.render(
     <BrowserRouter>
         <Provider store={ store }>
-            <App />
-            <NotificationsPopup />
+            <SkeletonTheme baseColor="#BCE9BE" highlightColor="#C3F2C5">
+                <App />
+                <NotificationsPopup />
+            </SkeletonTheme>
         </Provider>
     </BrowserRouter>
 )
