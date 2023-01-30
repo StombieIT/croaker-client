dd18a3e42ece4340978484cb6253b9791f6376c4
StombieIT <stombie@yandex.ru>
2023-01-30 18:40:38 +0300

    refactor croaks api

--- src/api/croaksApi.ts
+++ src/api/croaksApi.ts
@@ -1,13 +1,32 @@
-import { ICroakDto } from "../models/ICroakDto"
-import { IPaginator } from "../models/IPaginator"
 import { api } from "./api"
+import { GetPaginatorApiMethod } from "../models/GetPaginatorApiMethod"
+import { ICroakDto } from "../models/ICroakDto"
+import { IIdentifiable } from "../models/IIdentifiable"
+import { IPaginator } from "../models/IPaginator"
+import { PageableParamDefault } from "../models/PageableParamDefault"
 
-export const getCroaksByUserId = (userId: number, page: number = 1, limit: number = 10): Promise<IPaginator<ICroakDto>> =>
-    api.get<IPaginator<ICroakDto>>(`/croaks/by-user-id/${userId}?page=${page}&limit=${limit}`)
+export const getCroaksByUserId: GetPaginatorApiMethod<ICroakDto, IIdentifiable> = ({
+    id,
+    page = PageableParamDefault.PAGE,
+    limit = PageableParamDefault.LIMIT
+}) =>
+    api.get<IPaginator<ICroakDto>>(`/croaks/by-user-id/${id}?page=${page}&limit=${limit}`)
         .then(response => response.data)
 
-export const getRepliesByUserId = (userId: number, page: number = 1, limit: number = 10): Promise<IPaginator<ICroakDto>> =>
-    api.get<IPaginator<ICroakDto>>(`/croaks/replies/by-user-id/${userId}?page=${page}&limit=${limit}`)
+export const getRepliesByUserId: GetPaginatorApiMethod<ICroakDto, IIdentifiable> = ({
+    id,
+    page = PageableParamDefault.PAGE,
+    limit = PageableParamDefault.LIMIT
+}) =>
+    api.get<IPaginator<ICroakDto>>(`/croaks/replies/by-user-id/${id}?page=${page}&limit=${limit}`)
+        .then(response => response.data)
+
+export const getLikesByUserId: GetPaginatorApiMethod<ICroakDto, IIdentifiable> = ({
+    id,
+    page = PageableParamDefault.PAGE,
+    limit = PageableParamDefault.LIMIT
+}) =>
+    api.get<IPaginator<ICroakDto>>(`/croaks/likes/by-user-id/${id}?page=${page}&limit=${limit}`)
         .then(response => response.data)
 
 export const getCroakById = (id: number): Promise<ICroakDto> =>
