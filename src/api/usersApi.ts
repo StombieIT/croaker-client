1fae04ce70c31aedb2535172e2959ae4c753c208
StombieIT <stombie@yandex.ru>
2023-02-01 01:05:54 +0300

    fix formatting and add new method in users api

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,4 +1,8 @@
 import { api } from "./api"
 import { IUser } from "../models/IUser"
 
-export const getUserByAuth = (): Promise<IUser> => api.get<IUser>("/users/by-auth").then(response => response.data)
+export const getUserByAuth = (): Promise<IUser> => api.get<IUser>("/users/by-auth")
+    .then(response => response.data)
+
+export const isFollowedToUserWithId = (id: number): Promise<boolean> => api.get<boolean>(`/users/is-followed-to/${userId}`)
+    .then(response => response.data)
