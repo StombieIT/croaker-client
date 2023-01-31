6f9760d14b2784cc0d1773b923275d48a245cdbc
StombieIT <stombie@yandex.ru>
2023-02-01 01:10:13 +0300

    fix profile saga business logic

--- src/business-logic/profile/profileSaga.ts
+++ src/business-logic/profile/profileSaga.ts
@@ -1,22 +1,47 @@
-import { takeEvery, call, all, put, StrictEffect } from "@redux-saga/core/effects"
+import { StrictEffect, takeLatest, call, put, all } from "@redux-saga/core/effects"
 import { PayloadAction } from "@reduxjs/toolkit"
-import { getProfileById } from "../../api/profilesApi"
-import { IProfile } from "../../models/IProfile"
+import { IProfileDto } from "../../models/IProfileDto"
 import { NotificationType } from "../../models/NotificationType"
 import { appendNotificationContainer } from "../notifications/notificationsSlice"
-import { fetchProfileById, setIsLoading, setProfile } from "./profileSlice"
+import { fetchFullProfileById, setIsLoading, setProfile } from "./profileSlice"
+import { FetchError } from "../../models/FetchError"
+import * as profilesApi from "../../api/profilesApi"
+import * as usersApi from "../../api/usersApi"
 
 export function *profileWatcher(): Generator<StrictEffect> {
-    yield takeEvery(fetchProfileById.type, profileWorker)
+    yield takeLatest(fetchFullProfileById.type, fetchFullProfileByIdWorker)
 }
 
-export function *profileWorker(action: PayloadAction<number>) {
+export function *fetchFullProfileByIdWorker(action: PayloadAction<number>) {
     yield put(setIsLoading(true))
     try {
-        const profile: IProfile = yield call(getProfileById, action.payload)
-        yield put(setProfile(profile))
+        const [profile, followIsActive]: [IProfileDto, boolean] = yield all([
+            call(fetchProfileByIdWorker, action.payload),
+            call(fetchProfileFollowIsActiveByIdWorker, action.payload)
+        ])
+        yield put(setProfile({...profile, follow: {isLoading: false, isActive: followIsActive}}))
         yield put(setIsLoading(false))
     } catch (err) {
-        yield put(appendNotificationContainer({type: NotificationType.ERROR, text: "Unable to load profile"}))
+        if (err instanceof FetchError) {
+            yield put(appendNotificationContainer({type: NotificationType.ERROR, text: err.message}))
+        }
+    }
+}
+
+export function *fetchProfileByIdWorker(id: number) {
+    try {
+        const profile: IProfileDto = yield call(profilesApi.getProfileById, id)
+        return profile
+    } catch (err) {
+        throw new FetchError("Unable to get follow info ;(")
+    }
+}
+
+export function *fetchProfileFollowIsActiveByIdWorker(id: number) {
+    try {
+        const followIsActive: boolean = yield call(usersApi.isFollowedToUserWithId, id)
+        return followIsActive
+    } catch (err) {
+        throw new FetchError("Unable to get follow info ;(")
     }
 }
