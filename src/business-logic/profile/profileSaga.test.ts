9d01d1974a2fcb6e06ca421ce1f269525a9796e1
StombieIT <stombie@yandex.ru>
2023-02-01 14:34:47 +0300

    implement one more test in profile saga

--- src/business-logic/profile/profileSaga.test.ts
+++ src/business-logic/profile/profileSaga.test.ts
@@ -1,6 +1,13 @@
 import { FetchError } from "../../models/FetchError"
 import { IProfileDto } from "../../models/IProfileDto"
-import { fetchProfileByIdWorker } from "./profileSaga"
+import {
+    fetchProfileByIdWorker,
+    fetchProfileFollowIsActiveByIdWorker
+} from "./profileSaga"
+
+const generateBoolean = (): boolean => {
+    return Math.random() >= 0.5
+}
 
 describe("fetch profile by id worker tests", () => {
     let workerGenerator: ReturnType<typeof fetchProfileByIdWorker>
@@ -14,6 +21,26 @@ describe("fetch profile by id worker tests", () => {
         const next = workerGenerator.next(fakeProfile)
         expect(next.done).toBe(true)
         expect(next.value).toBe(fakeProfile)
+    })
+
+    test("unsuccessful fetching", () => {
+        workerGenerator.next()
+        expect(() => workerGenerator.throw(new Error())).toThrow(FetchError)
+    })
+})
+
+describe("fetch profile follow is active by id worker tests", () => {
+    let workerGenerator: ReturnType<typeof fetchProfileFollowIsActiveByIdWorker>
+    beforeEach(() => {
+        workerGenerator = fetchProfileFollowIsActiveByIdWorker(Math.random())
+    })
+    
+    test("successful fetching", () => {
+        workerGenerator.next()
+        const fakeFollow = generateBoolean()
+        const next = workerGenerator.next(fakeFollow)
+        expect(next.done).toBe(true)
+        expect(next.value).toBe(fakeFollow)
     })
 
     test("unsuccessful fetching", () => {
