2660bf14bf4d7eee3cd52c2ec55d2f0f228768b2
StombieIT <stombie@yandex.ru>
2023-02-02 15:08:05 +0300

    partly implement follow button

--- src/components/ProfileInteractionBar/ProfileInteractionBar.tsx
+++ src/components/ProfileInteractionBar/ProfileInteractionBar.tsx
@@ -1,23 +1,53 @@
 import { FC } from "react"
+import { useDispatch } from "react-redux"
+import { toggleProfileFollowIsActive } from "../../business-logic/profile/profileSlice"
+import { IActivable } from "../../models/IActivable"
+import { ILoadable } from "../../models/ILoadable"
 import { IUser } from "../../models/IUser"
+import { AppDispatch } from "../../store"
 import { Avatar } from "../Avatar/Avatar"
 import { Button, ButtonSubType } from "../Button/Button"
 
 import classes from "./ProfileInteractionBar.module.scss"
 
-interface IProfileInteractionBarProps {
-    user: IUser
+interface IProfileInteractionBarContainerProps {
+    user: IUser,
+    follow: ILoadable & IActivable
 }
 
-export const ProfileInteractionBar: FC<IProfileInteractionBarProps> = ({user}) => {
+interface IProfileInteractionBarProps extends IProfileInteractionBarContainerProps {
+    toggleFollowIsActive: () => void
+}
+
+export const ProfileInteractionBar: FC<IProfileInteractionBarProps> = ({user, follow, toggleFollowIsActive}) => {
     return <div className={ classes.bar }>
         <Avatar
             className={ classes.avatar }
             src={ user.avatarLink ?? undefined }
             alt={ `${user.username} avatar` }
         />
-        <Button className={ classes.button } subType={ ButtonSubType.PRIMARY }>
-            Follow
+        <Button
+            className={ classes.button }
+            subType={ ButtonSubType.PRIMARY }
+            disabled={ follow.isLoading }
+            onClick={ toggleFollowIsActive }
+        >
+            {
+                follow.isActive
+                ? "Unfollow"
+                : "Follow"
+            }
         </Button>
     </div>
 }
+
+export const ProfileInteractionBarContainer: FC<IProfileInteractionBarContainerProps> = props => {
+    const dispatch: AppDispatch = useDispatch()
+
+    return <ProfileInteractionBar
+        {...props}
+        toggleFollowIsActive={ () => dispatch(toggleProfileFollowIsActive()) }
+    />
+}
+
+export default ProfileInteractionBarContainer
