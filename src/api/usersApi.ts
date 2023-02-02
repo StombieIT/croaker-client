36c10065236a44336a5a4fb7c2f874998ed6fda5
StombieIT <stombie@yandex.ru>
2023-02-02 15:02:47 +0300

    add follow and unfollow methods and refactor

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,8 +1,18 @@
 import { api } from "./api"
 import { IUser } from "../models/IUser"
 
-export const getUserByAuth = (): Promise<IUser> => api.get<IUser>("/users/by-auth")
-    .then(response => response.data)
+export const getUserByAuth = (): Promise<IUser> =>
+    api.get<IUser>("/users/by-auth")
+        .then(response => response.data)
 
-export const isFollowedToUserWithId = (id: number): Promise<boolean> => api.get<boolean>(`/users/is-followed-to/${userId}`)
-    .then(response => response.data)
+export const isFollowedToUserWithId = (id: number): Promise<boolean> =>
+    api.get<boolean>(`/users/is-followed-to/${id}`)
+        .then(response => response.data)
+
+export const followToUserWithId = (id: number): Promise<boolean> =>    
+    api.put<boolean>(`/users/follow-to/${id}`)
+        .then(response => response.data)
+
+export const unfollowToUserWithId = (id: number): Promise<boolean> =>
+    api.put<boolean>(`/users/unfollow-to/${id}`)
+        .then(response => response.data)
