af7eb7acddcf75c33e4333d65ed60f577f502724
StombieIT <stombie@yandex.ru>
2023-02-02 15:09:35 +0300

    add two profile selectors

--- src/business-logic/profile/profileSelectors.ts
+++ src/business-logic/profile/profileSelectors.ts
@@ -1,4 +1,11 @@
+import { IActivable } from "../../models/IActivable"
+import { ILoadable } from "../../models/ILoadable"
+import { IUser } from "../../models/IUser"
 import { RootState } from "../../store"
 import { IProfileState } from "./profileSlice"
 
 export const selectProfileState = (state: RootState): IProfileState => state.profile
+
+export const selectProfileUser = (state: RootState): IUser | undefined => state.profile.profile?.user
+
+export const selectProfileFollow = (state: RootState): ILoadable & IActivable | undefined => state.profile.profile?.follow
