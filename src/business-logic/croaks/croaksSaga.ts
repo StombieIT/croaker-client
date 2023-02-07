abd0abd43a7fc0561741051b2ee32d74083ff6f1
StombieIT <stombie@yandex.ru>
2023-02-07 15:33:55 +0300

    add toggle like saga worker

--- src/business-logic/croaks/croaksSaga.ts
+++ src/business-logic/croaks/croaksSaga.ts
@@ -5,21 +5,25 @@ import { fromDto, ICroak } from "../../models/ICroak"
 import { ICroakDto } from "../../models/ICroakDto"
 import { NotificationType } from "../../models/NotificationType"
 import { appendNotificationContainer } from "../notifications/notificationsSlice"
-import { selectCroaksPaginator } from "./croaksSelectors"
+import { selectCroakLikesIsActiveById, selectCroaksPaginator } from "./croaksSelectors"
 import {
     fetchNextCroaksByUserId, fetchNextRepliesByUserId,
     fetchNextLikesByUserId, fetchOriginalCroakById,
     mergePaginator, setIsLoading, garbleOriginalCroak,
+    toggleLikesIsActiveByCroakId, setLikesIsLoadingByCroakId,
+    setLikesByCroakId
 } from "./croaksSlice"
 import { GetPaginatorApiMethod } from "../../models/GetPaginatorApiMethod"
 import { IIdentifiable } from "../../models/IIdentifiable"
 import * as croaksApi from "../../api/croaksApi"
+import { IReactionDto } from "../../models/IReactionDto"
 
 export function *croaksWatcher() {
     yield takeLatest(fetchNextCroaksByUserId.type, fetchNextCroaksByUserIdWorker)
     yield takeLatest(fetchNextRepliesByUserId.type, fetchNextRepliesByUserIdWorker)
     yield takeLatest(fetchNextLikesByUserId.type, fetchNextLikesByUserIdWorker)
     yield takeLeading(fetchOriginalCroakById.type, fetchOriginalCroakByIdWorker)
+    yield takeLeading(toggleLikesIsActiveByCroakId.type, toggleLikesIsActiveByCroakIdWorker)
 }
 
 export function *fetchNextByIdWorker(action: PayloadAction<number>, getByUserId: GetPaginatorApiMethod<ICroakDto, IIdentifiable>) {
@@ -53,5 +57,28 @@ export function *fetchOriginalCroakByIdWorker(action: PayloadAction<number>) {
         yield put(garbleOriginalCroak(fromDto(croak)))
     } catch (err) {
         yield put(appendNotificationContainer({type: NotificationType.ERROR, text: "Unable to load original croak ;("}))    
+    }
+}
+
+export function *toggleLikesIsActiveByCroakIdWorker(action: PayloadAction<number>) {
+    const croakLikesIsActive: boolean | undefined = yield select(selectCroakLikesIsActiveById(action.payload))
+    if (croakLikesIsActive !== undefined) {
+        yield put(setLikesIsLoadingByCroakId({id: action.payload, isLoading: true}))
+        try {
+            let reaction: IReactionDto
+            if (croakLikesIsActive) {
+                reaction = yield call(croaksApi.removeLikeByCroakId, action.payload)
+            } else {
+                reaction = yield call(croaksApi.addLikeByCroakId, action.payload)
+            }
+            yield put(setLikesByCroakId({id: action.payload, likes: reaction}))
+        } catch (err) {
+            yield put(appendNotificationContainer({
+                type: NotificationType.ERROR,
+                text: `Unable to ${croakLikesIsActive ? "un" : ""}like`
+            }))
+        } finally {
+            yield put(setLikesIsLoadingByCroakId({id: action.payload, isLoading: false}))
+        }
     }
 }
