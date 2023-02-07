87b6631656724788fee378429570d087783eea54
StombieIT <stombie@yandex.ru>
2023-02-07 21:39:47 +0300

    add skeleton profile meta

---
+++ src/components/ProfileMeta/SkeletonProfileMeta.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+import Skeleton from "react-loading-skeleton"
+
+import classes from "./ProfileMeta.module.scss"
+
+enum SkeletonProfileMetaUnit {
+    COMMON_HEIGHT = 15,
+    USER_ID_WIDTH = 70,
+    CITY_WIDTH = 100,
+    REGISTRATION_DATE_WIDTH = 120
+}
+
+export const SkeletonProfileMeta: FC = () => {
+    return <div className={ classes.container }>
+        <Skeleton width={ SkeletonProfileMetaUnit.USER_ID_WIDTH } height={ SkeletonProfileMetaUnit.COMMON_HEIGHT } />
+        <Skeleton width={ SkeletonProfileMetaUnit.CITY_WIDTH } height={ SkeletonProfileMetaUnit.COMMON_HEIGHT } />
+        <Skeleton width={ SkeletonProfileMetaUnit.REGISTRATION_DATE_WIDTH } height={ SkeletonProfileMetaUnit.COMMON_HEIGHT } />
+    </div>
+}
