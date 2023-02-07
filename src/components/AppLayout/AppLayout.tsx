1709f349d5a519b18da4dab96e6062daf7afaf0f
StombieIT <stombie@yandex.ru>
2023-02-07 16:54:05 +0300

    fix app layout imports

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
-import { Outlet, Navigate } from "react-router-dom"
-import { SideBar } from "../SideBar/SideBar"
+import { Outlet } from "react-router-dom"
+import SideBar from "../SideBar/SideBar"
 import { NarrowColumn } from "../NarrowColumn/NarrowColumn"
 
 import classes from "./AppLayout.module.scss"
