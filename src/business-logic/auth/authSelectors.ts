3e01bb0b202468666581a86555926fc3fa9900c9
StombieIT <stombie@yandex.ru>
2023-02-07 16:51:31 +0300

    add auth user id selector

--- src/business-logic/auth/authSelectors.ts
+++ src/business-logic/auth/authSelectors.ts
@@ -6,3 +6,5 @@ export const selectAuthState = (state: RootState): IAuthState => state.auth
 export const selectIsUnauthorized = (state: RootState): boolean => !state.auth.isLoading && !state.auth.user
 
 export const selectIsAuthorized = (state: RootState): boolean => !state.auth.isLoading && !!state.auth.user
+
+export const selectAuthUserId = (state: RootState): number | undefined => state.auth.user?.id
