dbd746e9fa5c91bb3252bbb1f3081a01b4aa6cd5
StombieIT <stombie@yandex.ru>
2023-02-07 15:34:46 +0300

    add select croak likes is active selector

--- src/business-logic/croaks/croaksSelectors.ts
+++ src/business-logic/croaks/croaksSelectors.ts
@@ -23,3 +23,20 @@ export const selectOriginalCroakOfCroakById = (id: number) => (state: RootState)
         ?.items
         .find(croak => croak.id === id)
         ?.originalCroak
+
+export const selectCroakLikesIsActiveById = (id: number) => (state: RootState): boolean | undefined =>
+    // trying to search between 'main' croaks
+    state.croaks
+        .paginator
+        ?.items
+        .find(croak => croak.id === id)
+        ?.likes.isActive ??
+    // trying to search between 'original' croaks
+    state.croaks
+        .paginator
+        ?.items
+        .map(croak => croak.originalCroak)
+        .filter(croak => croak)
+        .find(croak => croak?.id === id)
+        ?.likes
+        ?.isActive
