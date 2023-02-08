46cdb16c5c3366b87014f23a465a52faeb3d5550
StombieIT <stombie@yandex.ru>
2023-02-08 13:44:29 +0300

    implement profile username

---
+++ src/components/ProfileUsername/ProfileUsername.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react"
+import { useSelector } from "react-redux"
+import { selectProfileUser } from "../../business-logic/profile/profileSelectors"
+import { SkeletonUsername } from "../Username/SkeletonUsername"
+import { Username } from "../Username/Username"
+
+interface IProfileUsernameProps {
+    username: string
+}
+
+export const ProfileUsername: FC<IProfileUsernameProps> = ({username}) => {
+    return <Username>
+        { username }
+    </Username>
+}
+
+const ProfileUsernameContainer: FC = () => {
+    const profileUsername = useSelector(selectProfileUser)
+    
+    if (!profileUsername) {
+        return <SkeletonUsername width={ 70 } />
+    }
+
+    return <ProfileUsername
+        {...profileUsername}
+    />
+}
+
+export default ProfileUsernameContainer
