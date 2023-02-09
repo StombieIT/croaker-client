113f5dc74c4ceda11ccb2913ee4845e38d682d24
StombieIT <stombie@yandex.ru>
2023-02-09 16:07:43 +0300

    fix formatting

--- src/api/profilesApi.ts
+++ src/api/profilesApi.ts
@@ -1,5 +1,6 @@
 import { api } from "./api"
 import { IProfileDto } from "../models/IProfileDto"
 
-export const getProfileById = (id: number) => api.get<IProfileDto>(`/profiles/${id}`)
-    .then(response => response.data)
+export const getProfileById = (id: number) =>
+    api.get<IProfileDto>(`/profiles/${id}`)
+        .then(response => response.data)
