93eb53378ebe0f406500d2c9012dd93ae16b1da1
StombieIT <stombie@yandex.ru>
2023-02-09 16:57:39 +0300

    fix skeleton profile description component

--- src/components/ProfileDescription/SkeletonProfileDescription.tsx
+++ src/components/ProfileDescription/SkeletonProfileDescription.tsx
@@ -5,7 +5,7 @@ import { OrdinaryParagraph } from "../OrdinaryParagraph/OrdinaryParagraph"
 export const SkeletonProfileDescription: FC = () => {
     return <OrdinaryParagraph>
         <Skeleton />
-        <Skeleton width={ 450 } />
-        <Skeleton width={ 500 } />
+        <Skeleton />
+        <Skeleton />
     </OrdinaryParagraph>
 }
